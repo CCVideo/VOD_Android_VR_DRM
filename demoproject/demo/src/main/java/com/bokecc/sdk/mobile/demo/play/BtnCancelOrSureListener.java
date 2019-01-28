ae2189dbf6e29857bf0ead15f362750540ca5473
Han Ruisong <550399108@qq.com>
2019-01-28 14:07:34 +0800

    升级到v2.18.0
    1. SDK新增双字幕切换功能；
    2. Demo新增了下载重试次数和重试间隔的设置；
    3.SDK将默认下载重试次数改为10次，默认重试间隔改为3000ms；
    4.播放本地加密视频的方法由setDRMVideoPath改为setOfflineVideoPath；

---
+++ demoproject/demo/src/main/java/com/bokecc/sdk/mobile/demo/play/BtnCancelOrSureListener.java
@@ -0,0 +1,10 @@
+package com.bokecc.sdk.mobile.demo.play;
+
+/**
+ * Created by Administrator on 2017/7/14.
+ */
+
+public interface BtnCancelOrSureListener {
+    void btnSure(String verifyCode);
+    void btnCancel();
+}
---
+++ demoproject/demo/src/main/java/com/bokecc/sdk/mobile/demo/play/CustomDialogInputVerifyCode.java
@@ -0,0 +1,71 @@
+package com.bokecc.sdk.mobile.demo.play;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.bokecc.sdk.mobile.demo.R;
+
+
+public class CustomDialogInputVerifyCode extends Dialog {
+    private Context context;
+    private BtnCancelOrSureListener btnCancelOrSureListener;
+
+    public CustomDialogInputVerifyCode(@NonNull Context context, BtnCancelOrSureListener btnCancelOrSureListener) {
+        super(context, R.style.CustomDialogInputVerifyCode);
+        this.context = context;
+        this.btnCancelOrSureListener = btnCancelOrSureListener;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        init();
+    }
+
+    private void init() {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.dialog_input_verify_code, null);
+        setContentView(view);
+        TextView tv_cancel = (TextView) view.findViewById(R.id.tv_cancel);
+        TextView tv_sure = (TextView) view.findViewById(R.id.tv_sure);
+        final EditText et_input_verify_code = view.findViewById(R.id.et_input_verify_code);
+
+        tv_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btnCancelOrSureListener != null) {
+                    btnCancelOrSureListener.btnCancel();
+                    dismiss();
+                }
+            }
+        });
+
+        tv_sure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btnCancelOrSureListener != null) {
+                    btnCancelOrSureListener.btnSure(et_input_verify_code.getText().toString());
+                    dismiss();
+                }
+            }
+        });
+
+        Window dialogWindow = getWindow();
+        WindowManager.LayoutParams lp = dialogWindow.getAttributes();
+        DisplayMetrics d = context.getResources().getDisplayMetrics();
+        lp.width = (int) (d.widthPixels * 0.9);
+//        lp.height = (int) (d.heightPixels * 0.75);
+        dialogWindow.setAttributes(lp);
+
+
+    }
+}
