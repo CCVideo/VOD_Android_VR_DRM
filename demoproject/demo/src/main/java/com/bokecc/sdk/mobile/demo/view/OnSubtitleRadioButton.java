92a311b79e2d420ebc671c2596feee63e247b2e7
Han Ruisong <550399108@qq.com>
2019-05-17 10:34:36 +0800

    删除旧版本准备升级

--- demoproject/demo/src/main/java/com/bokecc/sdk/mobile/demo/view/OnSubtitleRadioButton.java
+++
@@ -1,8 +0,0 @@
-package com.bokecc.sdk.mobile.demo.view;
-
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-
-public interface OnSubtitleRadioButton {
-    void getRadioButtons(RadioButton fisrtSub,RadioButton secondSub,RadioButton twoSub);
-}
